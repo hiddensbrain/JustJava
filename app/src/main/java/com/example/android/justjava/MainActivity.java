11f0c56fd23ddf09c4531435b357ea5c468a860f
hiddensbrain <hiddensmaster@gmail.com>
2020-07-31 12:53:07 +0700

    first checkbox added

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -4,21 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.CheckBox;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     int quantity = 0;
     int coffeePrice = 5;
-    String name = "Paul";
+    String customerName = "Paul";
     String drinkOfTheDay = "latte";
-    String drinksOrdered = "Nothing";
+    double backPressedTime = 0;
+    Toast backToast;
+    boolean hasWhippedCream;
+    boolean hasCinnamon;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //next method stretches the application to fill whole phone screen
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
 
@@ -38,7 +50,9 @@ public class MainActivity extends AppCompatActivity {
     //This method is called when "order" button is clicked.
 
     public void submitOrder(View view) {
-        createOrderSummary(quantity, coffeePrice, name);
+        CheckBox whippedCreamCheckBox = findViewById(R.id.checkBoxCream);
+        hasWhippedCream = whippedCreamCheckBox.isChecked();
+        createOrderSummary(quantity, coffeePrice, customerName);
     }
 
     //This method is called when "reset" button is clicked.
@@ -65,18 +79,45 @@ public class MainActivity extends AppCompatActivity {
      * and else - method displays given texts in both price_text_view and order_text_view fields
      */
 
-    private void createOrderSummary(int quantity, int onePrice, String name) {
+    private void createOrderSummary(int quantity, int coffeePrice, String customerName) {
         TextView priceTextView = findViewById(R.id.price_text_view);
         TextView orderTextView = findViewById(R.id.price_text_primary);
-        int price = quantity * onePrice;
+        int price = quantity * coffeePrice;
+        String toppings;
+        if (hasWhippedCream) {
+            toppings = "Toppings: Whipped Cream";
+        } else {
+            toppings = "Toppings: None";
+        }
         if (quantity == 0) {
             priceTextView.setText("");
             orderTextView.setText("");
         } else {
-            String summary = "Name: " + name + "\n" + "Quantity: " + quantity + "\n" + "Total, $: " + price + "\n" + "Thank you!";
+            String summary = "Name: " + customerName + "\n" + "Quantity: " + quantity + "\n" + "Total, $: " + price + "\n" + toppings + "\n" + "\n" + "Thank you!";
             orderTextView.setText("Your Order:");
             priceTextView.setText(summary);
         }
 
+
+
     }
+
+    //system button "back" start
+    //Exiting app by popup and second click
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            backToast.cancel();
+            super.onBackPressed();
+            return;
+        } else {
+            backToast = Toast.makeText(getBaseContext(), "For Exit, Push one more time", Toast.LENGTH_SHORT);
+            backToast.show();
+        }
+        backPressedTime = System.currentTimeMillis();
+    }
+    //system button "back" end
 }
+
+
+
